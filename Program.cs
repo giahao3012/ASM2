f2ec6d3fd64a76354f9e45680c3a60568bceb838
= <=>
2021-02-11 15:24:18 +0700

    add ASM2

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            uint age;
+            String gender;
+            DateTime birthday;
+            string adress;
+            string email;
+            string fathername;
+            string mothername;
+
+            Console.WriteLine("Enter information: ");
+            Console.WriteLine("------***----------");
+            Console.Write("Enter student name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter gender (male/female):");
+            gender = Console.ReadLine();
+            Console.Write("Enter age: ");
+            age = uint.Parse(Console.ReadLine());
+            Console.Write("Enter birthday (MM/dd/yyyy): ");
+            birthday = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Enter Address: ");
+            adress = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            email = Console.ReadLine();
+            Console.Write("Enter Fathername: ");
+            fathername = Console.ReadLine();
+            Console.Write("Enter Mothername: ");
+            mothername = Console.ReadLine();
+
+            Console.WriteLine("*** Student Information ****");
+            Console.WriteLine("Student name: "+name);
+            Console.WriteLine("Student age: " +age);
+            Console.WriteLine("Student gender: " +gender);
+            Console.WriteLine("Date of Birth: " +birthday);
+            Console.WriteLine("Address: " +adress);
+            Console.WriteLine("Email: " +email);
+            Console.WriteLine("Student's Father name: " +fathername);
+            Console.WriteLine("Student's Mother name: " +mothername);
+
+            Console.ReadLine();
+        }
+    }
+}
